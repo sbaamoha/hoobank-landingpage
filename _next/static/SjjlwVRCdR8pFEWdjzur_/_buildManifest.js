b8151dab559284cd6ba5e080f093c5d4d68c7329
sbaamoha <sbaamoha@users.noreply.github.com>
2022-08-29 15:01:15 +0000

    deploy: 2639f2a0f0c3ff680fd5acdaf351f86c844d0c4c

--- _next/static/SjjlwVRCdR8pFEWdjzur_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-e1a4727575ad540d.js","static/chunks/pages/index-010dd5daaa621cf5.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
